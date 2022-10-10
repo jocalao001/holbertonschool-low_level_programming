6c36923eb9044c478b8e1360424ab614dc16a085
jose tirado <joca-001@hotmail.com>
2022-10-09 21:53:44 -0700

    3-print_numbers

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -7,8 +7,10 @@
  *       used in the 0x03-more-functions_nested_loops directory.
  */
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
 
 #endif

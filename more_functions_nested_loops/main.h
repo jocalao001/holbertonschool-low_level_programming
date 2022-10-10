dbec3e1d8ac827238081252cda78472174308546
jose tirado <joca-001@hotmail.com>
2022-10-09 21:37:43 -0700

    0-isupper

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x03-more-functions_nested_loops directory.
+ */
+
+int _isupper(int c);
+
+#endif

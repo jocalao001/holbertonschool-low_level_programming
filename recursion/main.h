6395b442274104fc2b9b95a1b3d797863392df91
jose tirado <joca-001@hotmail.com>
2022-10-21 20:55:50 -0700

    0-puts_recursion

---
+++ recursion/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Desc: Header file containing declarations for all functions
+ *       used in the Recursion directory.
+ */
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+/*
+ * Desc: containing  functions
+ *       used in the  directory.
+ */
+
+#endif

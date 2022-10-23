3f927208a13651ce14353b8a7c75a3ab0ddcef05
jose tirado <joca-001@hotmail.com>
2022-10-23 16:38:57 -0700

    0-whatsmyname

---
+++ argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Desc: Header file containing declarations for the  function
+ *    _putchar  used in the argc_argv  directory.
+ */
+
+int _putchar(char c);
+
+#endif

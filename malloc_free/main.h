f419eeca5b07d6885cb74fea0f79da1bcd703ced
jose tirado <joca-001@hotmail.com>
2022-10-26 17:21:12 -0700

    0-create_array

---
+++ malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Desc: Header file containing declarations for the  function
+ * used in the malloc_free  directory.
+ */
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+
+#endif

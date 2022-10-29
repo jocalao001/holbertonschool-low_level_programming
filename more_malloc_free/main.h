2ece14f1b07b97307a08642be5336bdf0cd11f9d
jose tirado <joca-001@hotmail.com>
2022-10-28 19:41:45 -0700

    0-malloc_checked

---
+++ more_malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Desc: Header file containing declarations for the  function
+ * used in the more_malloc_free  directory.
+ */
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+
+#endif
